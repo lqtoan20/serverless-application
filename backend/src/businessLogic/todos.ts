81c9671e91757947b44db40b2c2c69df1b07e217
ToanLa <lqtoan20@gmail.com>
2023-04-04 19:10:03 +0700

    update code

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -7,9 +7,18 @@ import { getUserId } from '../lambda/utils'
 import { createLogger } from '../utils/logger'
 import * as createError from 'http-errors'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
+import * as AWS from 'aws-sdk'
+import * as AWSXRay from 'aws-xray-sdk'
 
 const todosAccess = new TodosAccess()
 const logger = createLogger('todos')
+const XAWS = AWSXRay.captureAWS(AWS)
+const bucketName = process.env.ATTACHMENT_S3_BUCKET
+// To fix issue: The expiration must be a number, received string
+const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
+const s3 = new XAWS.S3({
+  signatureVersion: 'v4'
+})
 
 export async function createTodo(
   event: APIGatewayProxyEvent
@@ -105,4 +114,26 @@ export async function updateTodo(
     },
     body: JSON.stringify(todo)
   }
+}
+
+export async function createAttachmentPresignedUrl(
+  event: APIGatewayProxyEvent
+): Promise<string> {
+  logger.info('Start upload file')
+  const userId = getUserId(event)
+  const todoId = event.pathParameters.todoId
+  const attachmentId = uuid.v4()
+  if (!todoId) {
+    throw new createError.NotFound(`Todo item not found with id ${todoId}`)
+  }
+
+  const uploadUrl = s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: attachmentId,
+    Expires: urlExpiration
+  })
+
+  await todosAccess.updateAttachmentUrl(todoId, attachmentId, userId)
+
+  return uploadUrl
 }
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
-import { createAttachmentPresignedUrl } from '../../businessLogic/attachmentUpload'
+import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
 import { createLogger } from '../../utils/logger'
 const logger = createLogger('uploadTodoHandler')
 
